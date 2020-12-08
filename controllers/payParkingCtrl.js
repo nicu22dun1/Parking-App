de17142a9e820d29cbe8730038f3ddc9d41af78f
Hincu Nicusor <hincunicusor94@gmail.com>
2020-12-08 02:30:18 +0200

    finishing touch
    clean code and add annotations

--- controllers/payParkingCtrl.js
+++ controllers/payParkingCtrl.js
@@ -29,14 +29,6 @@ payParking.controller('payParkingController', function payParkingController($sco
         'parkingLotName'   : $scope.parkingLotName
     };
 
-    $scope.booleanEnterForm    = false;
-    $scope.booleanLeaveForm    = false;
-    $scope.showSuccessEnter    = false;
-    $scope.showSuccessLeave    = false;
-    $scope.showInvalidNumber   = false;
-    $scope.showCarNumberExists = false;
-    $scope.showSummary         = false;
-
     // regex for Bucharest and other counties
     var checkBucharestRegex = '^[bB]{1}[0-9]{2,3}[A-z]{3}';
     var checkCountiesRegex  = '^[A-z]{2}[0-9]{2}[A-z]{3}';
@@ -46,13 +38,18 @@ payParking.controller('payParkingController', function payParkingController($sco
     $scope.showLeaveForm            = showLeaveForm;
     $scope.occupyParkingLotSpace    = occupyParkingLotSpace;
     $scope.emptyParkingLotSpace     = emptyParkingLotSpace;
-    // $scope.availableParkingSpacesNo = getAvailableParkingSpotsNr();
     $scope.clearData                = clearData;
     $scope.updateParkingLotSpaces   = updateParkingLotSpaces;
     $scope.closeSummary             = closeSummary;
     $scope.confirmConfig            = confirmConfig;
 
     // Functions
+
+    /**
+     * Get available parking lot space.
+     * 
+     * @returns {number}/null Parking lot Id
+     */
     function getAvailableParkingLotSpaceId(){
         var keys = localStorageService.keys();
         for( i = 1; i <= localStorageService.get(0).totalParkingLots; i++ ){
@@ -62,23 +59,34 @@ payParking.controller('payParkingController', function payParkingController($sco
 
         return null;
     };
-
+    
+    /**
+     * Get parking lot space Id for specified license plate.
+     * 
+     * @param {string} carNumber
+     * @returns {number}/null Parking lot space Id 
+     */
     function getParkingLotSpaceIdForCarNumber (carNumber){
         var keys = localStorageService.keys();
         var parkingLotId = '';
         keys.forEach(function(item, index){
-            if(item != 0)
-                if(localStorageService.get(item).carNumber == carNumber)
+            if( item != 0 )
+                if( localStorageService.get(item).carNumber == carNumber )
                     parkingLotId = item; // return the parking lot space ID for the car to be removed
         });
 
         return parkingLotId??null;
     };
 
+    /**
+     * Go through stored data to get parking spaces details.
+     * 
+     * @returns {object} Parking lot spaces.
+     */
     function getParkingLotSpaces(){
         var parkingSpaces = [];
         for( i = 1; i <= localStorageService.get(0).totalParkingLots; i++ ){
-            if( localStorageService.keys().includes(i.toString()) ){ // check if parking space is ocupied when iterating
+            if( localStorageService.keys().includes( i.toString() ) ){ // check if parking space is ocupied when iterating
                 parkingSpaces.push({
                     'carNumber' : localStorageService.get(i).carNumber,
                     'entryDate' : localStorageService.get(i).entryDate
@@ -94,39 +102,54 @@ payParking.controller('payParkingController', function payParkingController($sco
         return parkingSpaces;
     };
 
+    /**
+     * @returns {number} No. of available parking spots.
+     */
     function getAvailableParkingSpotsNr(){
-        return localStorageService.get(0).totalParkingLots - (localStorageService.length() - 1);
+        return localStorageService.get(0).totalParkingLots - ( localStorageService.length() - 1 );
     };
 
+    /**
+     * Set parking lot spaces in scope.
+     * Set available number of parking spots in scope.
+     */
     function updateParkingLotSpaces(){
         $scope.parkingLotSpaces = getParkingLotSpaces();
         $scope.availableParkingSpacesNo = getAvailableParkingSpotsNr();
     };
 
+    /**
+     * Shows the parking entry form.
+     * Hides the parking leave form.
+     * Clears messages.
+     */
     function showEnterForm (){
         $scope.booleanEnterForm = true;
         $scope.booleanLeaveForm = false;
 
-        if( $scope.showSuccessEnter == true )
-            $scope.showSuccessEnter = !$scope.showSuccessEnter;
-
-        if( $scope.showSuccessLeave == true )
-            $scope.showSuccessLeave = false;
+        $scope.message = '';
     };
 
+    /**
+     * Shows the parking leave form.
+     * Hides the parking entry form.
+     * Clears messages.
+     */
     function showLeaveForm (){
         $scope.booleanLeaveForm = true;
         $scope.booleanEnterForm = false;
 
-        if( $scope.showSuccessEnter == true )
-            $scope.showSuccessEnter = false;
-
-        if( $scope.showSuccessLeave == true )
-            $scope.showSuccessLeave = false;
-
         $scope.message = '';
     };
 
+    /**
+     * Occupy a parking lot space for a specified license plate.
+     * Updates parking lot spaces from storage.
+     * Hides the parking entry form if successfull.
+     * Updates messages.
+     * 
+     * @param {string} carNumber
+     */
     function occupyParkingLotSpace (carNumber){
         carNumber = carNumber.toUpperCase();
         var licensePlateRegex = chooseRegex(carNumber);
@@ -136,7 +159,7 @@ payParking.controller('payParkingController', function payParkingController($sco
                 $scope.message = 'Masina cu numarul de inmatriculare '+ carNumber +' exista deja in parcare! Va rugam verificati numarul introdus.';
                 $scope.messageType = false;
             } else {
-                var date = Math.round(new Date().getTime() / 1000); // get current date
+                var date = Math.round( new Date().getTime() / 1000 ); // get current date
                 var parkingLotSpaceId = getAvailableParkingLotSpaceId();
                 localStorageService.add(parkingLotSpaceId, 
                     {
@@ -158,6 +181,14 @@ payParking.controller('payParkingController', function payParkingController($sco
         updateParkingLotSpaces();
     };
 
+    /**
+     * Empty the parking lot space for a specified license plate.
+     * Updates parking lot spaces from storage.
+     * Hides the parking leave form if successfull.
+     * Updates messages.
+     * 
+     * @param {string} carNumber 
+     */
     function emptyParkingLotSpace (carNumber){
         carNumber = carNumber.toUpperCase();
         var licensePlateRegex = chooseRegex(carNumber);
@@ -184,19 +215,40 @@ payParking.controller('payParkingController', function payParkingController($sco
 
     };
 
+    /**
+     * Clears storage and reinitialises the application.
+     */
     function clearData(){
         localStorageService.clearAll();
         location.reload();
     };
 
+    /** 
+     * Set correct Regex for a license plate format.
+     * 
+     * @param {string} carNumber
+     * 
+     * @returns {string} Regex
+     */
     function chooseRegex(carNumber){
         return carNumber.substr(1,1).search(/[0-9]/) != -1 ? checkBucharestRegex : checkCountiesRegex;
     };
 
+    /**
+     * Calculates time elapsed since entry in parking lot space.
+     * Calculates total invoice value.
+     * Creates message for calculated duration.
+     * 
+     * @param {UNIX Timestamp} entryTime 
+     * @param {string} carNumber 
+     * @param {number} parkingLotSpaceId
+     * 
+     * @returns {object} Summary
+     */
     function calculateSummary(entryTime, carNumber, parkingLotSpaceId){
-        var priceFirstHour = $scope.priceFirstHour;
-        var pricePerHour   = $scope.pricePerHour;
-        var date           = Math.round(new Date().getTime() / 1000); // get current date for summary in seconds
+        var priceFirstHour = localStorageService.get(0).priceFirstHour;
+        var pricePerHour   = localStorageService.get(0).pricePerHour;
+        var date           = Math.round( new Date().getTime() / 1000 ); // get current date for summary in seconds
         
         var summary               = {};
         summary.entryDate         = timeConverterUnixToDate(entryTime);
@@ -205,7 +257,7 @@ payParking.controller('payParkingController', function payParkingController($sco
         summary.parkingLotName    = $scope.parkingLotName;
         summary.parkingLotSpaceId = parkingLotSpaceId;
 
-        var minutesElapsed = Math.ceil((date - entryTime) / 60); // no. minutes
+        var minutesElapsed = Math.ceil( (date - entryTime) / 60 ); // no. minutes
         if( minutesElapsed <= 60 ){
             summary.totalInvoiceValue = priceFirstHour;
             if( minutesElapsed == 1 ){
@@ -214,8 +266,8 @@ payParking.controller('payParkingController', function payParkingController($sco
                 summary.subsequentTimeString = minutesElapsed.toString() + ' minute';
             }
         } else {
-            var elapsedHours = Math.floor(minutesElapsed / 60);
-            var subsequentMinutes = Math.ceil(minutesElapsed % 60);
+            var elapsedHours = Math.floor( minutesElapsed / 60 );
+            var subsequentMinutes = Math.ceil( minutesElapsed % 60 );
 
             summary.subsequentTimeString = elapsedHours.toString() + ' ore ' + (subsequentMinutes == 0 ? 'si 0 minute' : ( 'si ' + subsequentMinutes.toString() + ' minute'));
             summary.totalInvoiceValue = priceFirstHour + ( Math.ceil((minutesElapsed - 60) / 60) * pricePerHour );
@@ -224,6 +276,13 @@ payParking.controller('payParkingController', function payParkingController($sco
         return summary;
     };
 
+    /**
+     * Converts UNIX timestamp to readable format.
+     * 
+     * @param {UNIX Timestamp} UNIX_timestamp
+     * 
+     * @returns {string} Detailed time
+     */
     function timeConverterUnixToDate(UNIX_timestamp){
         var a      = new Date(UNIX_timestamp * 1000);
         var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
@@ -239,12 +298,22 @@ payParking.controller('payParkingController', function payParkingController($sco
 
     };
 
+    /**
+     * Closes the summary.
+     * Clears the summary object.
+     * Updates message.
+     */
     function closeSummary(){
         $scope.showSummary = false;
         $scope.summary     = '';
         $scope.message     = '';
     };
 
+    /**
+     * Closes the config.
+     * Stores config object with data inserted.
+     * Updates parking lot spaces.
+     */
     function confirmConfig(){
         $scope.showConfig = false;
         $scope.parkingLotConfig = {
@@ -255,7 +324,7 @@ payParking.controller('payParkingController', function payParkingController($sco
         };
         
         localStorageService.add('0', $scope.parkingLotConfig);
-        
+
         updateParkingLotSpaces();
 
     };
